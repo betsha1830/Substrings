89d10665def1c319b41b4dae2b96c43d004f7199
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-21 22:57:18 +0300

    Code clean up

--- substrings.rb
+++ substrings.rb
@@ -1,13 +1,3 @@
-# PROBLEM
-# How many substrings are there from the user input and the dictionary provided.
-# (if a word is found as a root word a prefixed or suffixed word that also counts).
-
-# CASES
-# 1- Takes two input ( 1- The word to be checked if it exists in the dictionary. 2- The dictionary itself)
-
-# SOLUTION
-# 1- Check input by iterating against the dictionary if the input matches as a word (eg input: low, dictionary: below. => this is a match)
-
 def substrings (input, dictionary)
     
     input.downcase!
@@ -22,7 +12,3 @@ def substrings (input, dictionary)
         count
     end
 end
-
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
-puts substrings "Howdy partner, sit down! How's it going?", dictionary
