1b412046733194e23e3ae633b526516a36ece667
Betsue Weldemariam <btsegay0@gmail.com>
2022-03-29 13:04:12 +0300

    Code refractor done using rubocop.

--- substrings.rb
+++ substrings.rb
@@ -1,14 +1,11 @@
-def substrings (input, dictionary)
-    
-    input.downcase!
+def substrings(input, dictionary)
+  input.downcase!
 
-    dictionary.reduce(Hash.new(0)) do |count, item|
-        
-        item.scan(input) do 
-            count[input] += 1
-            count[item] += 1 if (item != input)
-        end
-        input.scan(item) {count[item] += 1}
-        count
+  dictionary.each_with_object(Hash.new(0)) do |item, count|
+    item.scan(input) do
+      count[input] += 1
+      count[item] += 1 if item != input
     end
+    input.scan(item) { count[item] += 1 }
+  end
 end
